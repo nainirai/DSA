d5889386ff896b045017fef435e1e0ab412d26c6
Naini Rai <128786091+nainirai@users.noreply.github.com>
2023-05-21 14:47:49 +0530

    Create 41_First_Missing_Positive.java

---
+++ ARRAYS/41_First_Missing_Positive.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+         int n=nums.length;
+        int ans=n+1;
+        int vis[]=new int[n+1];
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>0 && nums[i]<=n)
+                vis[nums[i]]=1;        //This is done to mark the number nums[i] as visited.
+        }
+        for(int i=1;i<vis.length;i++)
+        {
+            if(vis[i]==0)
+            {
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+
+    // int[] nums = {3, 1, -2, 6, 4};
+    // vis = [0, 1, 0, 1, 1]
+    // thats why we started 2nd for loop from i+1 as 0th index will always be 0 in visited    array .
+
+}
