b66a85d472082ae928ff577dd07543cb12d32870
Naini Rai <128786091+nainirai@users.noreply.github.com>
2023-06-16 10:43:17 +0530

    238_ProductofArrayExceptSelf.java

--- ARRAYS/ProductofArrayExceptSelf.java
+++ ARRAYS/ProductofArrayExceptSelf.java
@@ -21,3 +21,49 @@ public int[] productExceptSelf(int[] nums) {
        }
        return ans;
     }
+
+// BRUTE FORCE O(N^2)
+    public int[] productExceptSelf(int[] nums){
+        int n =nums.length;
+        int ans[]=new int[n];
+
+        for(int i=0;i<n;i++){
+            int prod=1;
+            for(int j=0;j<n;j++){
+                if(i!=j){
+                     prod*=nums[j];
+                }  
+            }
+             ans[i]=prod;
+        }     
+        return ans;
+    }
+
+// DIVIDE PRODUCT WITH NUMBER  O(N) with space 0(1)
+    public int[] productExceptSelf(int[] nums){
+        int n=nums.length;
+        int ans[]=new int[n];
+        int prod=1;
+        int zerocount=0;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+              zerocount++;
+            }
+            else{
+                  prod*=nums[i];
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(zerocount>=2){
+                ans[i]=0;
+            }
+            else if(zerocount==1){
+                ans[i]=(nums[i]!=0) ? 0:prod;
+            }
+            else{
+                 ans[i]= (nums[i]!=0) ? prod / nums[i]:0;
+            }
+        }
+        return ans;
+    }
