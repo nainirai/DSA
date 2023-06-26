333e31cdb31f155c7e3652f46e9ad3e0861edd5c
Naini Rai <128786091+nainirai@users.noreply.github.com>
2023-06-26 18:28:56 +0530

    Create 1_Two_Sum.java

---
+++ ARRAYS/1_Two_Sum.java
@@ -0,0 +1,12 @@
+ public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int compl=target-nums[i];
+            if(map.containsKey(compl)){
+                return new int[]{map.get(compl), i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{-1,-1};
+
+    }
