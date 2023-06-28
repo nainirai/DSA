fd7b8b92c47a26a0fda4521631ff9039cb0a6345
Naini Rai <128786091+nainirai@users.noreply.github.com>
2023-06-28 16:49:17 +0530

    Update 26_Remove_Duplicates_from_Sorted_Array.java

--- ARRAYS/26_Remove_Duplicates_from_Sorted_Array.java
+++ ARRAYS/26_Remove_Duplicates_from_Sorted_Array.java
@@ -9,5 +9,8 @@ class Solution {
             }
         }
         return i + 1;
+         //  the task is to return the length of the modified array, 
+        // i + 1 is returned. Adding 1 accounts for the fact that i is a zero-based index, 
+        // and the length is one more than the index of the last element.
     }
 }
