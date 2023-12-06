2b8f4083afe4a6887caa4c16867682a23cd04f2d
Naini Rai <128786091+nainirai@users.noreply.github.com>
2023-12-06 16:11:40 +0530

    Update 100_Same Tree.java

--- BINARY_TREE/100_Same Tree.java
+++ BINARY_TREE/100_Same Tree.java
@@ -24,3 +24,23 @@
         return true;
     }
 }
+
+
+EASY WAY 
+
+ class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+   
+        if(p == null && q==null){
+            return true;
+        }
+        if(p==null || q==null || p.val!=q.val){
+            return false;
+        }
+        if(p.val == q.val){
+            
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+   return true;
+  }
+}
