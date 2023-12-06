149b06c68f1e766cebc3ed1f28d1f14e93de3066
Naini Rai <128786091+nainirai@users.noreply.github.com>
2023-12-06 12:29:33 +0530

    Create 100_Same Tree.java

---
+++ BINARY_TREE/100_Same Tree.java
@@ -0,0 +1,26 @@
+ public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p.val == q.val){
+            if(isIdentical(p, q)){
+                return true;
+            }            
+        }
+        boolean Lhs = isSameTree(p.left, q);
+        boolean Rhs = isSameTree(p.right, q);
+        return Lhs || Rhs;
+    }
+    public static boolean isIdentical(TreeNode p, TreeNode q){
+        if(p == null && q==null){
+            return true;
+        }
+        if(p==null || q==null || p.val!=q.val){
+            return false;
+        }
+        if(!isIdentical(p.left, q.left)){
+            return false;
+        }
+        if(!isIdentical(p.right, q.right)){
+            return false;
+        }
+        return true;
+    }
+}
